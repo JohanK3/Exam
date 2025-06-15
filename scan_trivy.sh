17ccdc80a434e931bf0c934f390aa5159ea51aa4
Johan Karl KASSA KASSA <102430033+JohanK3@users.noreply.github.com>
2025-06-15 14:35:33 +0100

    Update scan_trivy.sh (JSON)

--- scan_trivy.sh
+++ scan_trivy.sh
@@ -13,7 +13,12 @@ fi
 # Scanner les images avec un timeout plus long
 for image_name in $images; do
     echo "Scan de l'image $image_name avec Trivy..."
-    # Générer un nom de fichier basé sur le nom du service (par exemple, exam-eureka-service -> trivy-eureka-service.txt)
+    # Générer un nom de fichier basé sur le nom du service (par exemple, exam-eureka-service -> trivy-eureka-service.json)
     service_name=$(echo "$image_name" | sed 's/exam-//g' | cut -d':' -f1)
-    trivy image --scanners vuln --exit-code 0 --timeout 15m0s "$image_name" > "trivy-${service_name}.txt"
+    # Utiliser --format json pour la sortie JSON
+    # Le --exit-code 0 permet au pipeline de ne pas échouer même si des vulnérabilités sont trouvées,
+    # pour le débogage. Retirez-le pour un comportement plus strict en production.
+    trivy image --scanners vuln --exit-code 0 --timeout 15m0s --format json "$image_name" > "trivy-${service_name}.json"
 done
+
+echo "✅ Tous les scans Trivy sont terminés."
