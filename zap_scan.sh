f5d61bb10d79d54401ec28f85026b2ab164479a8
karl <johankarlkassa@gmail.com>
2025-06-17 16:21:18 +0100

    Passer à ZAP Automation Framework et résoudre erreur de permission dans Jenkins

--- zap_scan.sh
+++ zap_scan.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-TARGET_URL=${1:-http://localhost:8090}   # URL par défaut
-REPORT_FILE_JSON=${2:-zap_report.json}   # Fichier de rapport par défaut
+TARGET_URL=${1:-http://localhost:8090}
+REPORT_FILE_JSON=${2:-zap_report.json}
 
 echo "🔍 Démarrage du scan ZAP sur ${TARGET_URL}..."
+
+# Ajuster les permissions du répertoire courant
+chmod -R a+w "$(pwd)"
 
 docker run --rm \
   --network host \
   -v "$(pwd):/zap/wrk/:rw" \
   ghcr.io/zaproxy/zaproxy:stable \
-  zap-baseline.py -t "$TARGET_URL" -J "$REPORT_FILE_JSON" # Sans -g gen.conf
+  zap.sh -cmd -autorun /zap/wrk/zap-automation.yml
 
 echo "✅ Scan terminé. Rapport disponible: ${REPORT_FILE_JSON}"
